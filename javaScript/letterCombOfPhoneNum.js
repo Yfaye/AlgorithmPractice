a1dac0a46cfa0355d8e7615e9f94957436c32e83
Fei Yan <fayefei@gmail.com>
2016-05-03 17:24:47 -0700

    LeetCode17:Letter Combinations of a Phone Number
    Using DFS

--- javaScript/letterCombOfPhoneNum.js
+++ javaScript/letterCombOfPhoneNum.js
@@ -17,21 +17,59 @@ Although the above answer is in lexicographical order, your answer could be in a
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    var keyboard = [
-    	['a','b','c'],
-    	['d','e','f'],
-    	['g','h','i'],
-    	['j','k','l'],
-    	['m','n','o'],
-    	['p','q','r','s'],
-    	['t','u','v'],
-    	['w','x','y','z']
-    ]
-
-    var results = [];
-    var path = [];
-
-    var dfsHelper = function(keyboard, digits, results, path, pos) {
-    	
+    //corner case
+    if (digits === null || digits === undefined) {
+        return '';
     }
+    if (digits === '') {
+        return [];
+    }
+    //initialize
+    var path = [];
+    var results = [];
+    var digitsArr = digits.split('').map(function(n){return Number(n) - 2;});
+    //range check
+    if (digitsArr.some(function(n){return (n<0 || n>7) ;})){
+        return '';
+    }
+    
+    var keyboard = [
+                        ['a','b','c'],
+                        ['d','e','f'],
+                        ['g','h','i'],
+                        ['j','k','l'],
+                        ['m','n','o'],
+                        ['p','q','r','s'],
+                        ['t','u','v'],
+                        ['w','x','y','z']
+                    ];
+
+   var dfsHelper = function(keyboard, digitsArr, results, path, pos){
+       //base case
+       if (path.length === digitsArr.length) {
+           results.push(path.slice(0).join(''));
+           return;
+       }
+       if (pos > (digitsArr.length - 1)) {
+           return;
+       }
+       //recursive case
+       var index = digitsArr[pos];
+       for (var i = 0; i < keyboard[index].length; i++) {
+           path.push(keyboard[index][i]);
+           arguments.callee(keyboard, digitsArr, results, path, pos + 1);
+           path.pop(path.length - 1);
+       }
+   }
+   dfsHelper(keyboard, digitsArr, results, path, 0);
+   console.table(results);
+   return results;
 };
+letterCombinations('23');
+/*
+ad,ae,af,bd,be,bf,cd,ce,cf
+*/
+letterCombinations('2345');
+/*
+adgj,adgk,adgl,adhj,adhk,adhl,adij,adik,adil,aegj,aegk,aegl,aehj,aehk,aehl,aeij,aeik,aeil,afgj,afgk,afgl,afhj,afhk,afhl,afij,afik,afil,bdgj,bdgk,bdgl,bdhj,bdhk,bdhl,bdij,bdik,bdil,begj,begk,begl,behj,behk,behl,beij,beik,beil,bfgj,bfgk,bfgl,bfhj,bfhk,bfhl,bfij,bfik,bfil,cdgj,cdgk,cdgl,cdhj,cdhk,cdhl,cdij,cdik,cdil,cegj,cegk,cegl,cehj,cehk,cehl,ceij,ceik,ceil,cfgj,cfgk,cfgl,cfhj,cfhk,cfhl,cfij,cfik,cfil
+*/
