2dcb45504c1499fcf5a880872d1cd37efe3b78a0
Fei Yan <fayefei@gmail.com>
2016-06-28 08:36:44 +0800

    continue buy and sell

---
+++ javaScript/bestTimeToBuyII.js
@@ -0,0 +1,15 @@
+/*122. Best Time to Buy and Sell Stock II
+Total Accepted: 92585 Total Submissions: 214857 Difficulty: Medium
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    
+};
