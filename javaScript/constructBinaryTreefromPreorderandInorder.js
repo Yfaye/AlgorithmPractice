beed1ce218402d6a717a770fcfa8575b14319b0b
Fei Yan <fayefei@gmail.com>
2016-08-13 13:55:53 +0800

    还是MLE

--- javaScript/constructBinaryTreefromPreorderandInorder.js
+++ javaScript/constructBinaryTreefromPreorderandInorder.js
@@ -24,8 +24,8 @@ var buildTree = function(preorder, inorder) {
     if (preorder === null || inorder === null || preorder.length !== inorder.length) {
         return null;
     }
-    if (preorder.length === 0 || inorder.length === 0) {
-        return;
+    if (preorder.length === 0 || inorder.length === 0) { //这个是递归终止条件，一开始只有上下两个if，结果一直递归停不下来
+        return null;   
     }
 
     if (preorder.length === 1 && inorder.length === 1) {
@@ -35,41 +35,29 @@ var buildTree = function(preorder, inorder) {
 
 
     var rootNode = preorder[0];
-
     var rootAtInorder = inorder.indexOf(rootNode);
+
+    var left = null, right = null;
     var leftPreorder = [], rightPreorder = [], leftInorder = [], rightInorder = [];
     
-    if (rootAtInorder === 0) {
-        leftInorder = [];
-        rightInorder = inorder.slice(1);
-    } else if (rootAtInorder === 1) {
-        leftInorder = inorder.slice(0,1);
-        rightInorder = inorder.slice(2);
-    } else {
-        leftInorder = inorder.slice(0,rootAtInorder-1);
+    if (rootAtInorder === 0) { // 没有左子树
+        left = null;
         rightInorder = inorder.slice(rootAtInorder+1);
-    }
-    
-    var leftCount = leftInorder.length;
-    if (leftCount === 0) {
-        leftPreorder = [];
-        rightPreorder= preorder.slice(1);
-    } else if (leftCount === 1) {
-        leftPreorder = preorder.slice(1,2);
-        rightPreorder= preorder.slice(2);
+        rightPreorder = preorder.slice(1);
+        right = arguments.callee(rightPreorder, rightInorder);
+    } else if (rootAtInorder === inorder.length - 1) { // 没有右子树
+        right = null;
+        leftInorder = inorder.slice(0,rootAtInorder);
+        leftPreorder = preorder.slice(1);
     } else {
-       leftPreorder = preorder.slice(1, leftCount);
-       rightPreorder = preorder.slice(leftCount+1);        
+        leftInorder = inorder.slice(0,rootAtInorder);
+        rightInorder = inorder.slice(rootAtInorder+1);  
+        var leftCount = leftInorder.length;
+        leftPreorder = preorder.slice(1, leftCount+1);
+        rightPreorder = preorder.slice(leftCount+1);
+        left = arguments.callee(leftPreorder,leftInorder);
+        right = arguments.callee(rightPreorder, rightInorder);      
     }
-    console.log(leftPreorder);
-    console.log(leftInorder);
-    console.log(rightPreorder);
-    console.log(rightInorder);
-
-
-    
-    var left = arguments.callee(leftPreorder,leftInorder);
-    var right = arguments.callee(rightPreorder, rightInorder);
 
     var r = new TreeNode(rootNode);
     r.left = left;
@@ -77,10 +65,11 @@ var buildTree = function(preorder, inorder) {
 
     return r;
     
-    
 };
 
 // 贴过去没有AC，肉眼又暂时没有发现问题，决定写个测试跑一下
+// 因为对slice的用法不熟，所以debug了很久，slice的两个参数，生成的数组是不包含end那个所对应的值的
+
 function TreeNode(val) {
      this.val = val;
      this.left = this.right = null;
@@ -93,11 +82,53 @@ var test = buildTree(preOrder, inOrder);
 console.log(test);
 
 
+// 以上逻辑也对，能做出题目来，但是TLE和MLE了T_T苍天啊！！！！
+// 优化以后还是MLE
 
 
-/*
-undefined
-*/
-/*
-undefined
-*/
+var buildTree = function(preorder, inorder) {
+    if (preorder === null || inorder === null || preorder.length !== inorder.length) {
+        return null;
+    }
+    if (preorder.length === 0 || inorder.length === 0) { //这个是递归终止条件，一开始只有上下两个if，结果一直递归停不下来
+        return null;   
+    }
+
+    if (preorder.length === 1 && inorder.length === 1) {
+        var node = new TreeNode(preorder[0]);
+        return node;
+    }
+
+
+    var rootNode = preorder[0];
+    var rootAtInorder = inorder.indexOf(rootNode);
+
+    var left = null, right = null;
+    var leftPreorder = [], rightPreorder = [], leftInorder = [], rightInorder = [];
+    
+    if (rootAtInorder === 0) { // 没有左子树
+        left = null;
+        rightInorder = inorder.slice(rootAtInorder+1);
+        rightPreorder = preorder.slice(1);
+        right = arguments.callee(rightPreorder, rightInorder);
+    } else if (rootAtInorder === inorder.length - 1) { // 没有右子树
+        right = null;
+        leftInorder = inorder.slice(0,rootAtInorder);
+        leftPreorder = preorder.slice(1);
+        left = arguments.callee(leftPreorder,leftInorder);
+    } else {
+        leftInorder = inorder.slice(0,rootAtInorder);
+        rightInorder = inorder.slice(rootAtInorder+1);  
+        var leftCount = leftInorder.length;
+        leftPreorder = preorder.slice(1, leftCount+1);
+        rightPreorder = preorder.slice(leftCount+1);
+        left = arguments.callee(leftPreorder,leftInorder);
+        right = arguments.callee(rightPreorder, rightInorder);      
+    }
+
+    var r = new TreeNode(rootNode);
+    r.left = left;
+    r.right = right;
+
+    return r;
+};
