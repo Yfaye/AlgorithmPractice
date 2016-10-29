f5f4408ceed251b345b11b3bc4a029d0f796d716
Fei Yan <fayefei@gmail.com>
2016-10-29 08:59:43 +0800

    passed but still have bug

--- algs4/week4/8puzzle/Solver.java
+++ algs4/week4/8puzzle/Solver.java
@@ -155,26 +155,26 @@ public class Solver {
     }
 
     //solve a slider puzzle
-    public static void main(String[] args) {
-        // create initial board from file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        int[][] blocks = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                blocks[i][j] = in.readInt();
-        Board initial = new Board(blocks);
+    // public static void main(String[] args) {
+    //     // create initial board from file
+    //     In in = new In(args[0]);
+    //     int n = in.readInt();
+    //     int[][] blocks = new int[n][n];
+    //     for (int i = 0; i < n; i++)
+    //         for (int j = 0; j < n; j++)
+    //             blocks[i][j] = in.readInt();
+    //     Board initial = new Board(blocks);
 
-        // solve the puzzle
-        Solver solver = new Solver(initial);
+    //     // solve the puzzle
+    //     Solver solver = new Solver(initial);
 
-        // print solution to standard output
-        if (!solver.isSolvable())
-            StdOut.println("No solution possible");
-        else {
-            StdOut.println("Minimum number of moves = " + solver.moves());
-            for (Board board : solver.solution())
-                StdOut.println(board);
-        }       
-    }
+    //     // print solution to standard output
+    //     if (!solver.isSolvable())
+    //         StdOut.println("No solution possible");
+    //     else {
+    //         StdOut.println("Minimum number of moves = " + solver.moves());
+    //         for (Board board : solver.solution())
+    //             StdOut.println(board);
+    //     }       
+    // }
 }
