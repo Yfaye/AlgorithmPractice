75b0a183e749e3911704f7a704191767143df3e6
Yfaye <fayefei@gmail.com>
2022-04-22 14:35:08 +0800

    leetcode 706

---
+++ java/DesignHashmap.java
@@ -0,0 +1,91 @@
+/**
+ * LeetCode 706 https://leetcode.com/problems/design-hashmap/
+ */
+
+class MyHashMap {
+    private class Pair {
+        private int key;
+        private int value;
+    
+        public Pair(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+        
+        public int getKey() {
+            return key;
+        }
+        
+        public int getValue() {
+            return value;
+        }
+        
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
+    
+    private static final int BASE = 769;
+    private LinkedList[] data;
+    
+    // initilize the data structure in constructor
+    public MyHashMap() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; i++) {
+            data[i] = new LinkedList<Pair>();
+        }
+    }
+    
+    // inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+    public void put(int key, int value) {
+        int k = hash(key);
+        Iterator<Pair> iterator = data[k].iterator();
+        while (iterator.hasNext()) {
+            Pair pair = iterator.next();
+            if(pair.getKey() == key) {
+                pair.setValue(value);
+                return;
+            }
+        }
+        data[k].offerLast(new Pair(key, value));
+    }
+    
+    //  returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+    public int get(int key) {
+        int k = hash(key);
+        Iterator<Pair> iterator = data[k].iterator();
+        while (iterator.hasNext()) {
+            Pair pair = iterator.next();
+            if (pair.getKey() == key) {
+                return pair.value;
+            }
+        }
+        return -1;   
+    }
+    
+    // removes the key and its corresponding value if the map contains the mapping for the key.
+    public void remove(int key) {
+        int k = hash(key);
+        Iterator<Pair> iterator = data[k].iterator();
+        while(iterator.hasNext()) {
+            Pair pair = iterator.next();
+            if (pair.getKey() == key) {
+                data[k].remove(pair);
+                return;
+            }
+        }
+        
+    }
+    
+    private static int hash(int key) {
+        return key % BASE;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
