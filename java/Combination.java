847d222b55f2476e42a9fb380053ab98415f8b41
Yfaye <fayefei@gmail.com>
2022-04-29 12:36:37 +0800

    leetcode 77

---
+++ java/Combination.java
@@ -0,0 +1,27 @@
+/**
+ * LeetCode 77 https://leetcode.com/problems/combinations/submissions/
+ */
+
+class Solution {
+    List<List<Integer>> res = new LinkedList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        backtrack(n, k, 1);
+        return res;
+    }
+    
+    void backtrack(int n, int k, int startIndex) {
+        // base case
+        if (path.size() == k) {
+            res.add(new LinkedList(path));
+        }
+        
+        for (int i = startIndex; i <= n; i++ ) {
+            path.add(i);
+            
+            backtrack(n, k, i+1);
+            
+            path.removeLast();
+        }
+    }
+}
